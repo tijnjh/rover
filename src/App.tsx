269b7421f40d0d7cc421506ad01578f31b1a66ee
tijn <tijn@tijn.dev>
2025-05-19 15:55:26 +0200

    refactor: remove unused/random stuff

--- src/App.tsx
+++ src/App.tsx
@@ -9,36 +9,16 @@ import {
 	setupIonicReact,
 } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
-import { cog, search, settings, telescope } from "ionicons/icons";
+import { cog, search, telescope } from "ionicons/icons";
 import { Redirect, Route } from "react-router-dom";
 import Posts from "./pages/Posts";
-/* Core CSS required for Ionic components to work properly */
-import "@ionic/react/css/core.css";
 
-/* Basic CSS for apps built with Ionic */
+import "@ionic/react/css/core.css";
 import "@ionic/react/css/normalize.css";
 import "@ionic/react/css/structure.css";
 import "@ionic/react/css/typography.css";
-
-/* Optional CSS utils that can be commented out */
-import "@ionic/react/css/padding.css";
-import "@ionic/react/css/float-elements.css";
-import "@ionic/react/css/text-alignment.css";
-import "@ionic/react/css/text-transformation.css";
-import "@ionic/react/css/flex-utils.css";
-import "@ionic/react/css/display.css";
-
 import "./App.css";
 
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
 import "@ionic/react/css/palettes/dark.system.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import moment from "moment";
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,15 @@
-/// <reference types="vitest" />
-
-import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { fileURLToPath } from "node:url";
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
-		legacy(),
 		tailwindcss(),
 		tsconfigPaths({
 			root: fileURLToPath(new URL(".", import.meta.url)),
 		}),
 	],
-	test: {
-		globals: true,
-		environment: "jsdom",
-		setupFiles: "./src/setupTests.ts",
-	},
 });
