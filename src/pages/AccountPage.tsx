f8f98b6175c28c48b8ba32240e8f517aa94ec308
tijn <tijn@tijn.dev>
2025-06-18 22:31:26 +0200

    style: fmt

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -1,25 +1,25 @@
 import {
-    IonContent,
-    IonHeader,
-    IonPage,
-    IonTitle,
-    IonToolbar,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
 } from "@ionic/react";
 
 export default function AccountPage() {
-    return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Settings</IonTitle>
-                </IonToolbar>
-            </IonHeader>
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Settings</IonTitle>
+        </IonToolbar>
+      </IonHeader>
 
-            <IonContent color="light">
-                <div className="p-4">
-                    Account view is not implemented yet
-                </div>
-            </IonContent>
-        </IonPage>
-    );
+      <IonContent color="light">
+        <div className="p-4">
+          Account view is not implemented yet
+        </div>
+      </IonContent>
+    </IonPage>
+  );
 }
--- src/pages/MessagesPage.tsx
+++ src/pages/MessagesPage.tsx
@@ -1,25 +1,25 @@
 import {
-    IonContent,
-    IonHeader,
-    IonPage,
-    IonTitle,
-    IonToolbar,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
 } from "@ionic/react";
 
 export default function MessagesPage() {
-    return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Settings</IonTitle>
-                </IonToolbar>
-            </IonHeader>
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Settings</IonTitle>
+        </IonToolbar>
+      </IonHeader>
 
-            <IonContent color="light">
-                <div className="p-4">
-                    Messages view is not implemented yet
-                </div>
-            </IonContent>
-        </IonPage>
-    );
+      <IonContent color="light">
+        <div className="p-4">
+          Messages view is not implemented yet
+        </div>
+      </IonContent>
+    </IonPage>
+  );
 }
